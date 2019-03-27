9cd933420ab7dd8d3b132c56e2d5dbd174191572
uriegel <uriegel@hotmail.de>
2019-03-27 05:24:17 +0100

    accessing bridge from node-dotnet

--- node-dotnet/node-dotnet.js
+++ node-dotnet/node-dotnet.js
@@ -1,3 +1,11 @@
+const bridge = require('node-dotnet-bridge')
+
+const log = function(text) { console.log(text)}
+
 exports.test = function (text) { 
     console.log("Vom addon " + text)
+    bridge.initialize(log)
+    bridge.testLogging("Das was äber schön 👌😁")
+    bridge.unInitialize()
+    console.log("Finished")
 }
