10057797611e11b62809f4bd4772fad9e142a4dd
uriegel <uriegel@hotmail.de>
2019-04-06 11:07:16 +0200

    TestModules

---
+++ dev/TestModule/Processor.cs
@@ -0,0 +1,30 @@
+﻿using NodeDotnet.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModule
+{
+    [JavascriptObject]
+    public class Processor : IJavascriptObject
+    {
+        [JavascriptMethod]
+        public string GetTest(string text, double number, DateTime dateTime)
+            => $"Returned from CSharp: {text}, {number}, {dateTime}";
+        [JavascriptMethod]
+        public int Add(int a, int b) => a + b;
+
+        [JavascriptMethod]
+        public async Task<int> AddAsync(int a, int b)
+        {
+            await Task.Delay(1000);
+            return a + b;
+        }
+
+        public int Noop() => 9;
+
+        public string Execute(string method, string payload)
+            => "returned from C#: " + payload; 
+    }
+}
